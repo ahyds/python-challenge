5fee8705bdf672f07f42dd980e282fc765802a23
Sherry H <okul@qq.com>
2019-06-12 13:15:42 -0400

    Finished PyPoll-main.py,uploaded main.py and generated new.txt

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -37,7 +37,8 @@ for m in unique_list:
 #print(unique_list)
 #print(can_count)
 
-print(f"  Election Results"
+# Print poll statistics to terminal
+print(f"Election Results"
   f"\n-------------------------"
   f"\nTotal Votes: {cnt}\n-------------------------")
 
@@ -47,17 +48,16 @@ for x in range(len(unique_list)):
 print(f"-------------------------"
   f"\nWinner: {unique_list[can_count.index(max(can_count))]}\n-------------------------")
 
-# File for output
-fileout =open('new.txt','w+')
-fileout.write(f"  Election Results"
+
+# File path for output
+filepath = os.path.join('','','new.txt')
+
+with open(filepath,'w+') as fileout:
+  fileout.write(f"Election Results"
   f"\n-------------------------"
-  f"\nTotal Votes: {cnt}\n-------------------------"
-
-#filetxt    = os.path.join('','','new.txt')
-with open('new.txt','a') as filelpout:
-        for x in range(len(unique_list)):
-                filelpout.write(f"{unique_list[x]}: {can_count[x]/cnt:.3%} ({can_count[x]})")
-
-fileout =open('new.txt','w+')
-fileout.write(f"-------------------------"
-  f"\nWinner: {unique_list[can_count.index(max(can_count))]}\n-------------------------"))
+  f"\nTotal Votes: {cnt}\n-------------------------")
+  for x in range(len(unique_list)):
+    line = f"\n{unique_list[x]}: {can_count[x]/cnt:.3%} ({can_count[x]})"
+    fileout.write(line)
+  fileout.write(f"\n-------------------------"
+  f"\nWinner: {unique_list[can_count.index(max(can_count))]}\n-------------------------")
